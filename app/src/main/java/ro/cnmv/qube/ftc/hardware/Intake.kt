07d9020089234112ab9100136687290533b7282f
Andrei Codrin Daha <andrei.daha@cnmv.ro>
2018-12-26 23:09:08 +0200

    Fixed Intake class

--- app/src/main/java/ro/cnmv/qube/ftc/hardware/Intake.kt
+++ app/src/main/java/ro/cnmv/qube/ftc/hardware/Intake.kt
@@ -11,66 +11,67 @@ import org.bytedeco.javacpp.avformat
 
 class Intake (hwMap: HardwareMap) {
     val slideMotor = hwMap.dcMotor.get("slideMotor") ?: throw Exception("Failed to find motor slideMotor")
+    val rotateMotor = hwMap.dcMotor.get("rotateMotor") ?: throw Exception("Failed to find motor rotateMotor")
     val maturicaServo1 = hwMap.crservo.get("intakeServo1") ?: throw Exception("Failed to find crservo intakeServo1")
     val maturicaServo2 = hwMap.crservo.get("intakeServo2") ?: throw Exception("Failed to find crservo intakeServo2")
 
     init {
-        setSlideDirection(DcMotorSimple.Direction.FORWARD)
+        slideMotor.direction = DcMotorSimple.Direction.FORWARD
+        slideMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
         stopSlide()
-        resetSlideEncoder()
+        slideMotor.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        slideMotor.mode = DcMotor.RunMode.RUN_USING_ENCODER
 
-        setCrServoDirection(maturicaServo1, DcMotorSimple.Direction.FORWARD)
-        setCrServoDirection(maturicaServo2, DcMotorSimple.Direction.REVERSE)
+        rotateMotor.direction = DcMotorSimple.Direction.FORWARD
+        rotateMotor.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        stopRotate()
+        rotateMotor.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        rotateMotor.mode = DcMotor.RunMode.RUN_USING_ENCODER
+
+        maturicaServo1.direction = DcMotorSimple.Direction.FORWARD
+        maturicaServo2.direction = DcMotorSimple.Direction.REVERSE
         stopMaturica()
 
 
     }
 
-    /// SlideMotor functions
+    /// Slide Motor functions
     //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    //Set Direction
-    fun setSlideDirection(direction: DcMotorSimple.Direction) {
-        slideMotor.direction = direction
-    }
-
-    //Set Mode
-    fun setSlideMode(mode: DcMotor.RunMode) {
-        slideMotor.mode = mode
-    }
-
-    //Reset Encoder
-    fun resetSlideEncoder() {
-        slideMotor.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
-        slideMotor.mode = DcMotor.RunMode.RUN_USING_ENCODER
-    }
-
-    //Set Power
-    fun setSlidePower(power: Double) {
-        slideMotor.power = power
-    }
-
-    //Stop Motor
-    fun stopSlide() {
-        setSlidePower(0.0)
-    }
-
-    //Run To Position
     fun runSlideToPosition(position: Int, power: Double) {
         slideMotor.targetPosition = position
         slideMotor.power = power
     }
 
-    val OPEN_POSITION: Int = TODO()
-    val CLOSE_POSITION: Int =  TODO()
-    val TRANSFER_POSITION: Int = TODO()
-    val MODE_POWER: Double = TODO()
+    fun stopSlide() {
+        slideMotor.power = 0.0
+    }
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    /// Rotate Motor functions
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    fun runRotateToPosition(position: Int, power: Double) {
+        rotateMotor.targetPosition = position
+        rotateMotor.power = power
+    }
+
+    fun stopRotate() {
+        rotateMotor.power = 0.0
+    }
 
     enum class Mode{
         OPEN,
         CLOSE,
         TRANSFER
     }
+
+    val OPEN_POSITION: Int = TODO()
+    val CLOSE_POSITION: Int =  TODO()
+    val TRANSFER_POSITION: Int = TODO()
+    val MODE_POWER: Double = TODO()
 
     //Set Mode
     fun setMode(mode: Mode) {
@@ -79,41 +80,29 @@ class Intake (hwMap: HardwareMap) {
             Mode.CLOSE -> CLOSE_POSITION
             Mode.TRANSFER -> TRANSFER_POSITION
         }
-        runSlideToPosition(pos, MODE_POWER)
+        runRotateToPosition(pos, MODE_POWER)
     }
 
     //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-
-    /// CrServo functions
+    /// Maturica functions
     //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    //Set Direction
-    fun setCrServoDirection(servo: CRServo, direction: DcMotorSimple.Direction) {
-        servo.direction = direction
-    }
-
-    //Set Power
-    fun setCrServoPower(servo: CRServo, power: Double) {
-        servo.power = power
-    }
-
-    //Set System Power
     fun setMaturicaPower(power: Double) {
         maturicaServo1.power = power
         maturicaServo2.power = power
     }
 
-    //Stop
-    fun stopCrServo(servo: CRServo) {
-        setCrServoPower(servo, 0.0)
+    fun stopMaturica() {
+        maturicaServo1.power = 0.0
+        maturicaServo2.power = 0.0
     }
 
-    //Stop System
-    fun stopMaturica() {
-        stopCrServo(maturicaServo1)
-        stopCrServo(maturicaServo2)
+    val POWER = 1.0
+    fun maturica(af: Boolean) {
+        if (af) setMaturicaPower(POWER)
+        else setMaturicaPower(0.0)
     }
 
     //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
