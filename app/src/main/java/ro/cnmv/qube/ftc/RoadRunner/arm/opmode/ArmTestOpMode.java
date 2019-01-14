4dd7734b96fca9eab7d50ce93e6194c40a1e913b
Andrei Codrin Daha <andrei.daha@cnmv.ro>
2019-01-14 12:17:15 +0200

    Added QuickRoadRunner classes

---
+++ app/src/main/java/ro/cnmv/qube/ftc/RoadRunner/arm/opmode/ArmTestOpMode.java
@@ -0,0 +1,33 @@
+package ro.cnmv.qube.ftc.RoadRunner.arm.opmode;
+
+import com.acmerobotics.roadrunner.util.NanoClock;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import ro.cnmv.qube.ftc.RoadRunner.arm.Arm;
+
+/*
+ * Simple test of motion-profiled arm autonomous operation. The arm should move *smoothly*
+ * between random angles.
+ */
+@Autonomous
+public class ArmTestOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Arm arm = new Arm(hardwareMap);
+        NanoClock clock = NanoClock.system();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (!isStopRequested()) {
+            arm.setAngle(Arm.MAX_ANGLE * Math.random());
+
+            double startTime = clock.seconds();
+            while (!isStopRequested() && (clock.seconds() - startTime) < 5) {
+                arm.update();
+            }
+        }
+    }
+}
