28d561733c054a82377ba6b8fb80846e46c52532
Matei Chesa <chesamatei@yahoo.ro>
2019-01-19 11:24:57 +0200

    Add some initial code for autonomy

---
+++ app/src/main/java/ro/cnmv/qube/ftc/autonomy/AutonomyRedDepo.kt
@@ -0,0 +1,36 @@
+package ro.cnmv.qube.ftc.autonomy
+
+import ro.cnmv.qube.ftc.hardware.Hardware
+import ro.cnmv.qube.ftc.hardware.sensors.TFOD
+
+class AutonomyRedDepo: AutonomyBase() {
+    override fun preInit() {
+        // TODO: object detection
+        goldPosition = TFOD.Positions.LEFT
+    }
+
+    override fun Hardware.run() {
+        // TODO: de-latch
+
+        val trajectory = when (goldPosition) {
+            TFOD.Positions.LEFT -> arrayListOf(
+                Pair(15.0, 0.0),
+                Pair(70.0, 30.0),
+                Pair(-70.0, 30.0),
+                Pair(0.0, 60.0)
+            )
+            TFOD.Positions.RIGHT -> arrayListOf(
+                Pair(15.0, 0.0),
+                Pair(70.0, -30.0),
+                Pair(-70.0, -30.0),
+                Pair(0.0, 60.0)
+            )
+            else -> arrayListOf(
+                Pair(15.0, 0.0),
+                Pair(70.0, 0.0),
+                Pair(-70.0, 0.0),
+                Pair(0.0, 60.0)
+            )
+        }
+    }
+}
